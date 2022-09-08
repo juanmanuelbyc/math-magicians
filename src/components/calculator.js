3725427a295b3bc339da16d5ff418a9307047c4f
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-09-07 22:04:53 -0400

    fix linters errors

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Display = (props) => {
-  const value = props.value;
+  const {value} = props;
   return (
     <div className="display">
       <h1>{value}</h1>
     </div>
   );
-}
+};
 
 Display.defaultProps = {
   value: 0,
@@ -18,14 +18,14 @@ Display.propTypes = {
 };
 
 const CalculatorButton = (props) => {
-    const size = props.size;
-    const type = props.type;
-    const label = props.label;
-    const classes = `${size} ${type} button`;
-    return (
-      <button type="button" className={classes}>{label}</button>
-    );
-  };
+  const {size} = props;
+  const {type} = props;
+  const {label} = props;
+  const classes = `${size} ${type} button`;
+  return (
+    <button type="button" className={classes}>{label}</button>
+  );
+};
 
 CalculatorButton.propTypes = {
   size: PropTypes.string.isRequired,
@@ -34,30 +34,30 @@ CalculatorButton.propTypes = {
 };
 
 const ButtonsContainer = () => {
-    return (
-      <div className="buttonsContainer">
-        <CalculatorButton type="characterButton" size="s1" label="AC" />
-        <CalculatorButton type="characterButton" size="s1" label="+/-" />
-        <CalculatorButton type="characterButton" size="s1" label="%" />
-        <CalculatorButton type="operationButton" size="s1" label="÷" />
-        <CalculatorButton type="characterButton" size="s1" label="1" />
-        <CalculatorButton type="characterButton" size="s1" label="2" />
-        <CalculatorButton type="characterButton" size="s1" label="3" />
-        <CalculatorButton type="operationButton" size="s1" label="x" />
-        <CalculatorButton type="characterButton" size="s1" label="4" />
-        <CalculatorButton type="characterButton" size="s1" label="5" />
-        <CalculatorButton type="characterButton" size="s1" label="6" />
-        <CalculatorButton type="operationButton" size="s1" label="-" />
-        <CalculatorButton type="characterButton" size="s1" label="7" />
-        <CalculatorButton type="characterButton" size="s1" label="8" />
-        <CalculatorButton type="characterButton" size="s1" label="9" />
-        <CalculatorButton type="operationButton" size="s1" label="+" />
-        <CalculatorButton type="characterButton" size="s2" label="0" />
-        <CalculatorButton type="characterButton" size="s1" label="." />
-        <CalculatorButton type="operationButton" size="s1" label="=" />
-      </div>
-    );
-  };
+  return (
+    <div className="buttonsContainer">
+      <CalculatorButton type="characterButton" size="s1" label="AC" />
+      <CalculatorButton type="characterButton" size="s1" label="+/-" />
+      <CalculatorButton type="characterButton" size="s1" label="%" />
+      <CalculatorButton type="operationButton" size="s1" label="÷" />
+      <CalculatorButton type="characterButton" size="s1" label="1" />
+      <CalculatorButton type="characterButton" size="s1" label="2" />
+      <CalculatorButton type="characterButton" size="s1" label="3" />
+      <CalculatorButton type="operationButton" size="s1" label="x" />
+      <CalculatorButton type="characterButton" size="s1" label="4" />
+      <CalculatorButton type="characterButton" size="s1" label="5" />
+      <CalculatorButton type="characterButton" size="s1" label="6" />
+      <CalculatorButton type="operationButton" size="s1" label="-" />
+      <CalculatorButton type="characterButton" size="s1" label="7" />
+      <CalculatorButton type="characterButton" size="s1" label="8" />
+      <CalculatorButton type="characterButton" size="s1" label="9" />
+      <CalculatorButton type="operationButton" size="s1" label="+" />
+      <CalculatorButton type="characterButton" size="s2" label="0" />
+      <CalculatorButton type="characterButton" size="s1" label="." />
+      <CalculatorButton type="operationButton" size="s1" label="=" />
+    </div>
+  );
+};
 
 class Calculator extends React.Component {
   render() {
