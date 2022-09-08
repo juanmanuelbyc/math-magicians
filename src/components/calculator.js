c46762acbe8a14b6ed61780f3a678f53bacee5c2
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-09-07 22:21:41 -0400

    fix linters errors

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -32,36 +32,34 @@ CalculatorButton.propTypes = {
 };
 
 const ButtonsContainer = () => (
-    <div className="buttonsContainer">
-      <CalculatorButton type="characterButton" size="s1" label="AC" />
-      <CalculatorButton type="characterButton" size="s1" label="+/-" />
-      <CalculatorButton type="characterButton" size="s1" label="%" />
-      <CalculatorButton type="operationButton" size="s1" label="÷" />
-      <CalculatorButton type="characterButton" size="s1" label="1" />
-      <CalculatorButton type="characterButton" size="s1" label="2" />
-      <CalculatorButton type="characterButton" size="s1" label="3" />
-      <CalculatorButton type="operationButton" size="s1" label="x" />
-      <CalculatorButton type="characterButton" size="s1" label="4" />
-      <CalculatorButton type="characterButton" size="s1" label="5" />
-      <CalculatorButton type="characterButton" size="s1" label="6" />
-      <CalculatorButton type="operationButton" size="s1" label="-" />
-      <CalculatorButton type="characterButton" size="s1" label="7" />
-      <CalculatorButton type="characterButton" size="s1" label="8" />
-      <CalculatorButton type="characterButton" size="s1" label="9" />
-      <CalculatorButton type="operationButton" size="s1" label="+" />
-      <CalculatorButton type="characterButton" size="s2" label="0" />
-      <CalculatorButton type="characterButton" size="s1" label="." />
-      <CalculatorButton type="operationButton" size="s1" label="=" />
+  <div className="buttonsContainer">
+    <CalculatorButton type="characterButton" size="s1" label="AC" />
+    <CalculatorButton type="characterButton" size="s1" label="+/-" />
+    <CalculatorButton type="characterButton" size="s1" label="%" />
+    <CalculatorButton type="operationButton" size="s1" label="÷" />
+    <CalculatorButton type="characterButton" size="s1" label="1" />
+    <CalculatorButton type="characterButton" size="s1" label="2" />
+    <CalculatorButton type="characterButton" size="s1" label="3" />
+    <CalculatorButton type="operationButton" size="s1" label="x" />
+    <CalculatorButton type="characterButton" size="s1" label="4" />
+    <CalculatorButton type="characterButton" size="s1" label="5" />
+    <CalculatorButton type="characterButton" size="s1" label="6" />
+    <CalculatorButton type="operationButton" size="s1" label="-" />
+    <CalculatorButton type="characterButton" size="s1" label="7" />
+    <CalculatorButton type="characterButton" size="s1" label="8" />
+    <CalculatorButton type="characterButton" size="s1" label="9" />
+    <CalculatorButton type="operationButton" size="s1" label="+" />
+    <CalculatorButton type="characterButton" size="s2" label="0" />
+    <CalculatorButton type="characterButton" size="s1" label="." />
+    <CalculatorButton type="operationButton" size="s1" label="=" />
+  </div>
+);
+
+const Calculator = () => (
+    <div className="calculatorContainer">
+      <Display />
+      <ButtonsContainer />
     </div>
   );
-
-const Calculator = () => {
-    return (
-      <div className="calculatorContainer">
-        <Display />
-        <ButtonsContainer />
-      </div>
-    );
-};
 
 export default Calculator;
